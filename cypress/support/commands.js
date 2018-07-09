63731d1f1512ea456ce801b59a224b17244c5e35
Dominic Elm <dominic@thoughtram.io>
2018-07-09 11:54:16 +0200

    step 6: implement login command

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,3 +1,18 @@
 Cypress.Commands.add('getByTestId', id => {
   return cy.get(`[data-cy="${id}"]`);
 });
+
+Cypress.Commands.add('login', () => {
+  cy.visit('/login');
+  cy.clearLocalStorage();
+
+  cy.getByTestId('username')
+    .type('ppan')
+    .blur();
+
+  cy.getByTestId('password')
+    .type('ppan')
+    .blur();
+
+  cy.getByTestId('login-button').click();
+});
